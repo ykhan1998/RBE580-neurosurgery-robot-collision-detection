5b4953ef6cc354d14d6d05a96bc8c7a80244e69c
ykhan1998 <ykhan1998@hotmail.com>
2021-05-11 01:33:45 -0400

    cf

--- src/collide_free/scripts/collide_detect.py
+++ src/collide_free/scripts/collide_detect.py
@@ -26,18 +26,39 @@ def MRI_collide_detection(joint_pos):
     
     # radius of MRI bore
     radius_bore = 350
-    # TODO determine weight of each danger point colliding possibility to total
-    w1 = 0.3
-    w2 = 0.1
-    w3 = 0.25
-    w4 = 0.35
+    # determine weight of each danger point colliding possibility to total
+    w = [0.25,0.25,0.20,0.18,0.12]
     #define clearance distance
-    dis_safe = 25 #mm
-    # TODO define danger points
-    danger_point1 = np.matrix([0,0,0,1]).T
-    danger_point2 = np.matrix([0,0,0,1]).T
-    danger_point3 = np.matrix([0,0,0,1]).T
-    danger_point4 = np.matrix([0,0,0,1]).T
+    dis_safe = 20 #mm
+    # RCM rotation matrix
+    # Obtain basic Yaw, Pitch, and Roll Rotations
+    # TODO:check how the joint inputs are tracked (directions)
+    YawRotation = joint_pos[2]
+    PitchRotation = joint_pos[3]
+    ProbeRotation = joint_pos[5]
+    ProbeInsertion = joint_pos[4]
+    xDeltaRCM = joint_pos[0]
+    # Calculate the XYZ Rotation
+    xRotationDueToYawRotationFK = np.matrix([[1, 0, 0],[0, m.cos(YawRotation), -m.sin(YawRotation)],[0, m.sin(YawRotation), m.cos(YawRotation)]])
+    yRotationDueToPitchRotationFK = np.matrix([[m.cos(PitchRotation), 0, m.sin(PitchRotation)],[0, 1, 0],[-m.sin(PitchRotation), 0, m.cos(PitchRotation)]])
+    zRotationDueToProbeRotationFK = np.matrix([[m.cos(ProbeRotation), -m.sin(ProbeRotation), 0],[m.sin(ProbeRotation), m.cos(ProbeRotation), 0],[0, 0, 1]])
+    RCMRotation = np.identity(4)
+    RCMRotation[0:3,0:3] = np.matmul(np.matmul(xRotationDueToYawRotationFK,yRotationDueToPitchRotationFK) , zRotationDueToProbeRotationFK)
+    RCMRotation2D = np.identity(4)
+    RCMRotation2D[0:3,0:3] = np.matmul(xRotationDueToYawRotationFK,yRotationDueToPitchRotationFK)
+    # point 1: probe top-base
+    danger_point1 = np.matmul(RCMRotation,np.matrix([5,181.31+ProbeInsertion,0,1]).T)
+    # point 2: probe top-head
+    danger_point2 = np.matmul(RCMRotation,np.matrix([69.924,181.31+ProbeInsertion,0,1]).T)
+    # point 3: "The back bone"
+    danger_point3 = np.matmul(RCMRotation2D,np.matrix([-76.1,183.07,0,1]).T)
+    # point 4: "The butt"
+    danger_point4 = np.matrix([-254.0-xDeltaRCM,-77.983,0,1]).T
+    # point 5: "The elbow"
+    danger_point5 = np.matmul(RCMRotation2D,np.matrix([0,83,0,1]).T)
+    danger_point5[0] = danger_point5[0] - 188.132
+
+
     # Now we assume they are level in x and have sqrt((35**2) - (20**2)) in y
     ozx = 20
     ozy = m.sqrt((35**2) - (20**2))
@@ -45,22 +66,26 @@ def MRI_collide_detection(joint_pos):
     zFrameToTreatment, zFrameToRCM = kine.FK_Z_tip(joint_pos)
 
     # Convert danger points from RCM frame to Z frame
-    Z_point1 = np.matmul(zFrameToRCM,danger_point1)
-    Z_point2 = np.matmul(zFrameToRCM,danger_point2)
-    Z_point3 = np.matmul(zFrameToRCM,danger_point3)
-    Z_point4 = np.matmul(zFrameToRCM,danger_point4)
+    Z_points = []
+    Z_points.append(np.matmul(zFrameToRCM,danger_point1))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point2))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point3))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point4))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point5))
 
+
+    
     # calculate the distance from danger point to MRI bore
-    disC_p1 = radius_bore - m.sqrt((Z_point1[0] - ozx) ** 2 + (Z_point1[1] - ozy) ** 2)
-    disC_p2 = radius_bore - m.sqrt((Z_point2[0] - ozx) ** 2 + (Z_point2[1] - ozy) ** 2)
-    disC_p3 = radius_bore - m.sqrt((Z_point3[0] - ozx) ** 2 + (Z_point3[1] - ozy) ** 2)
-    disC_p4 = radius_bore - m.sqrt((Z_point4[0] - ozx) ** 2 + (Z_point4[1] - ozy) ** 2)
-
-    # collision factor calculation
-    if disC_p1 <= 0 or disC_p2 <= 0 or disC_p3 <= 0 or disC_p4 <= 0:
-        collision_factor = 1.
-    else:
-        collision_factor = (w1*(dis_safe - disC_p1) + w2*(dis_safe - disC_p2) + w3*(dis_safe - disC_p3) + w4*(dis_safe - disC_p4)) / dis_safe
+    collision_factor = 0
+    for idx,points in enumerate(Z_points):
+        disC= radius_bore - m.sqrt((points[0] - ozx) ** 2 + (points[1] - ozy) ** 2)
+        # collision factor calculation
+        if disC <= 0:
+            collision_factor = 1
+            break
+        else:
+            collision_factor = collision_factor + w[idx]*(dis_safe - disC) 
+    collision_factor = collision_factor/dis_safe
 
     return collision_factor
 
@@ -70,18 +95,36 @@ def Head_collide_detection(joint_pos):
 
     # radius of the head
     radius_head = 200
-    # TODO determine weight of each danger point colliding possibility to total
-    w1 = 0.3
-    w2 = 0.1
-    w3 = 0.25
-    w4 = 0.35
+    # determine weight of each danger point colliding possibility to total
+    w = [0.3,0.1,0.3,0.1,0.22]
     #define clearance distance
     dis_safe = 25 #mm
-    # TODO define danger points
-    danger_point1 = np.matrix([0,0,0,1]).T
-    danger_point2 = np.matrix([0,0,0,1]).T
-    danger_point3 = np.matrix([0,0,0,1]).T
-    danger_point4 = np.matrix([0,0,0,1]).T
+    # RCM rotation matrix
+    # Obtain basic Yaw, Pitch, and Roll Rotations
+    # TODO:check how the joint inputs are tracked (directions)
+    YawRotation = joint_pos[2]
+    PitchRotation = joint_pos[3]
+    ProbeRotation = joint_pos[5]
+    ProbeInsertion = joint_pos[4]
+    # Calculate the XYZ Rotation
+    xRotationDueToYawRotationFK = np.matrix([[1, 0, 0],[0, m.cos(YawRotation), -m.sin(YawRotation)],[0, m.sin(YawRotation), m.cos(YawRotation)]])
+    yRotationDueToPitchRotationFK = np.matrix([[m.cos(PitchRotation), 0, m.sin(PitchRotation)],[0, 1, 0],[-m.sin(PitchRotation), 0, m.cos(PitchRotation)]])
+    zRotationDueToProbeRotationFK = np.matrix([[m.cos(ProbeRotation), -m.sin(ProbeRotation), 0],[m.sin(ProbeRotation), m.cos(ProbeRotation), 0],[0, 0, 1]])
+    RCMRotation = np.identity(4)
+    RCMRotation[0:3,0:3] = np.matmul(np.matmul(xRotationDueToYawRotationFK,yRotationDueToPitchRotationFK) , zRotationDueToProbeRotationFK)
+    RCMRotation2D = np.identity(4)
+    RCMRotation2D[0:3,0:3] = np.matmul(xRotationDueToYawRotationFK,yRotationDueToPitchRotationFK)
+    # point 1: probe tip
+    # point 2: probe top-base
+    danger_point2 = np.matmul(RCMRotation,np.matrix([5,181.31+ProbeInsertion,0,1]).T)
+    # point 3: probe top-head
+    danger_point3 = np.matmul(RCMRotation,np.matrix([69.924,181.31+ProbeInsertion,0,1]).T)
+    # point 4: "The back bone"
+    danger_point4 = np.matmul(RCMRotation2D,np.matrix([-76.1,183.07,0,1]).T)
+    # point 5: "The back bone bottom"
+    danger_point5 = np.matmul(RCMRotation2D,np.matrix([-80.6,73.07,0,1]).T)
+
+
     # define the varance from head center to zframe in 3D
     # Now we assume the are level in x and have sqrt((35**2) - (20**2)) in y
     ozx = 0
@@ -92,21 +135,24 @@ def Head_collide_detection(joint_pos):
     zFrameToTreatment, zFrameToRCM = kine.FK_Z_tip(joint_pos)
 
     # Convert danger points from RCM frame to Z frame
-    Z_point1 = np.matmul(zFrameToRCM,danger_point1)
-    Z_point2 = np.matmul(zFrameToRCM,danger_point2)
-    Z_point3 = np.matmul(zFrameToRCM,danger_point3)
-    Z_point4 = np.matmul(zFrameToRCM,danger_point4)
+    Z_points = []
+    Z_points.append(zFrameToTreatment[:,3])
+    Z_points.append(np.matmul(zFrameToRCM,danger_point2))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point3))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point4))
+    Z_points.append(np.matmul(zFrameToRCM,danger_point5))
 
-    # calculate the distance from danger point to MRI bore
-    disC_p1 = radius_head - m.sqrt((Z_point1[0] - ozx) ** 2 + (Z_point1[1] - ozy) ** 2 + (Z_point1[2] - ozz) ** 2)
-    disC_p2 = radius_head - m.sqrt((Z_point2[0] - ozx) ** 2 + (Z_point2[1] - ozy) ** 2 + (Z_point2[2] - ozz) ** 2)
-    disC_p3 = radius_head - m.sqrt((Z_point3[0] - ozx) ** 2 + (Z_point3[1] - ozy) ** 2 + (Z_point3[2] - ozz) ** 2)
-    disC_p4 = radius_head - m.sqrt((Z_point4[0] - ozx) ** 2 + (Z_point4[1] - ozy) ** 2 + (Z_point4[2] - ozz) ** 2)
+    # calculate the distance from danger point to patient head
+    collision_factor = 0
+    for idx,point in enumerate(Z_points):
+        disC = radius_head - m.sqrt((point[0] - ozx) ** 2 + (point[1] - ozy) ** 2 + (point[2] - ozz) ** 2)
+        # collision factor calculation
+        if disC <= 0:
+            collision_factor = 1
+            break
+        else:
+            collision_factor = collision_factor + w[idx]*(dis_safe - disC)
 
-    # collision factor calculation
-    if disC_p1 <= 0 or disC_p2 <= 0 or disC_p3 <= 0 or disC_p4 <= 0:
-        collision_factor = 1.
-    else:
-        collision_factor = (w1*(dis_safe - disC_p1) + w2*(dis_safe - disC_p2) + w3*(dis_safe - disC_p3) + w4*(dis_safe - disC_p4)) / dis_safe
+    collision_factor = collision_factor/dis_safe
 
     return collision_factor
---
+++ src/collide_free/scripts/test2.py
@@ -0,0 +1,3 @@
+import numpy as np
+a = np.matrix([[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1]])
+print(a[:,3])
